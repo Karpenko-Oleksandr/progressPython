731957961f4d2d398da79d59024ec849f3837f33
OleksandrKarpenkoUA <1ooksapp8@gmail.com>
2024-12-17 08:53:32 +0200

    added to class Point getter and setter

--- main.py
+++ main.py
@@ -1,32 +1,18 @@
-class Circle:
-    def __init__(self, radius):
-        if not self.is_valid_radius(radius):
-            raise ValueError("Radius must be a positive number.")
-        self.radius = radius
+class Point:
+    def __init__(self, x=0, y=0):
+        self.__x = x
+        self.__y = y
 
-    def area(self):
-        return 3.14 * self.radius ** 2
+    def set_coord(self, x, y):
+        if type(x) in (int, float) and type(y) in (int, float):
+            self.__x = x
+            self.__y = y
+        else:
+            raise ValueError("Coords can be number type")
 
-    def circumference(self):
-        return 2 * 3.14 * self.radius
+    def get_coord(self):
+        return self.__x, self.__y
 
-    @classmethod
-    def from_diameter(cls, diameter):
-        if not cls.is_valid_radius(diameter / 2):
-            raise ValueError("Radius derived from diameter must be positive.")
-        return cls(diameter / 2)
-
-    @staticmethod
-    def is_valid_radius(value):
-        return isinstance(value, (int, float)) and value > 0
-
-
-try:
-    circle = Circle.from_diameter(10)
-    print(f"Radius: {circle.radius}")
-    print(f"Area: {circle.area()}")
-    print(f"Circumference: {circle.circumference()}")
-
-    print(Circle.is_valid_radius(-5))
-except ValueError as e:
-    print(e)
+test_point = Point(1, 2)
+test_point.set_coord(10, 20)
+print(test_point.get_coord())
