078e44321364f497f38bb413a3c81a7606ac137e
OleksandrKarpenkoUA <1ooksapp8@gmail.com>
2024-12-18 18:19:37 +0200

    realized pattern monostate

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
-class Point:
-    def __init__(self, x=0, y=0):
-        self.__x = x
-        self.__y = y
+class ThreadData:
+    __share_attrs = {
+        'name': 'thread_1',
+        'data': {},
+        'id': 1
+    }
 
-    def set_coord(self, x, y):
-        if type(x) in (int, float) and type(y) in (int, float):
-            self.__x = x
-            self.__y = y
-        else:
-            raise ValueError("Coords can be number type")
-
-    def get_coord(self):
-        return self.__x, self.__y
-
-test_point = Point(1, 2)
-test_point.set_coord(10, 20)
-print(test_point.get_coord())
+    def __init__(self):
+        self.__dict__= self.__share_attrs
